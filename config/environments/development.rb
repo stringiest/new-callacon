e0f9279b205a042cbcbdc8b4f9f67150a9b2b1dc
Lucy Stringer <lucyjstringer@gmail.com>
2023-11-15 21:26:04 +0000

    add empty line

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Define default url options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
