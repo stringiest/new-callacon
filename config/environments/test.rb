4f869d7bd1c03cfb4d6f9a41ae16d96234f56e60
Lucy Stringer <lucyjstringer@gmail.com>
2023-11-15 16:54:41 +0000

    run rails generate devise:install

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Set default url options - placeholder until set for production
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
