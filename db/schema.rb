356f45713cfde661a8e103c52c85f8a01bed5214
Lucy Stringer <lucyjstringer@gmail.com>
2024-11-17 08:32:34 +0000

    feat: add extra costs to bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_16_202827) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_17_083008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_16_202827) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "event_id", null: false
+    t.boolean "cancellable"
+    t.integer "assistance_donate"
+    t.boolean "assistance_claim"
+    t.boolean "single_person"
+    t.integer "dog"
     t.index ["event_id"], name: "index_bookings_on_event_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
